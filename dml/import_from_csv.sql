a50b21ab517d88e8161ce67c9e777eab00b3141d
Ilyazanuda <ilyazanuda@gmail.com>
2023-09-18 11:36:05 +0300

    second commit

--- dml/import_from_csv.sql
+++ dml/import_from_csv.sql
@@ -1,6 +1,6 @@
-copy articles from '.\data_kaggle\articles.csv' delimiter ',' csv header;
-copy transactions_train from '.\data_kaggle\transactions_train.csv' delimiter ',' csv header;
-copy customers from '.\data_kaggle\customers.csv' delimiter ',' csv header;
+copy articles from './articles.csv' delimiter ',' csv header;
+copy transactions_train from './transactions_train.csv' delimiter ',' csv header;
+copy customers from './customers.csv' delimiter ',' csv header;
 
 alter table transactions_train
 add column id serial primary key;
